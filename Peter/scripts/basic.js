d1b57608e7e3cc21ab81526b51acead81c2eacac
peru3232 <prw@qmail.com.de>
2022-01-27 10:02:29 +0100

    Basic 1

---
+++ Peter/scripts/basic.js
@@ -0,0 +1,7 @@
+// Exercise 1
+function crystalGazer(childNr, partnerName, location, jobTitle) {
+    console.log(`You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${childNr} childrens.`)
+    return true;
+}
+
+const print = crystalGazer(2, 'Jenny', 'Ostermiething', 'Freelancer');
