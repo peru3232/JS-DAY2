4640a432e1ee3c927cf7dfecb138215c7ddcecc7
peru3232 <prw@qmail.com.de>
2022-01-27 10:52:56 +0100

    Basic done

--- Peter/scripts/basic.js
+++ Peter/scripts/basic.js
@@ -1,7 +1,48 @@
 // Exercise 1
 function crystalGazer(childNr, partnerName, location, jobTitle) {
-    console.log(`You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${childNr} childrens.`)
-    return true;
+    console.log(`You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${childNr} childrens.`);
 }
 
-const print = crystalGazer(2, 'Jenny', 'Ostermiething', 'Freelancer');
+let print = crystalGazer(2, 'Jenny', 'Ostermiething', 'Freelancer');
+//------------------------------------------------------------------------------------------------------------------
+
+
+// Exercise 2
+function ageCalculator(birthYear, currentYear) {
+    const age = currentYear - birthYear;
+    console.log(`You are either ${age -1} or ${age}`);
+}
+
+print = ageCalculator(1993, 2022);
+//------------------------------------------------------------------------------------------------------------------
+
+
+// Exercise 3
+function ageCalculatorEasy(birthYear) {
+    const actualDate = new Date();
+    const age = actualDate.getFullYear() - birthYear;
+    console.log(`You are either ${age -1} or ${age}`);
+}
+
+print + ageCalculatorEasy(1993);
+//------------------------------------------------------------------------------------------------------------------
+
+
+// Exercise 4
+function degreesToRadiant(degrees) {
+    return Math.floor((degrees * (Math.PI/180) * 100000))/ 100000;
+}
+
+console.log(degreesToRadiant(90));
+//------------------------------------------------------------------------------------------------------------------
+
+
+//Exercise 5
+function calc(width, height, depth) {
+    const area = width * height;
+    return [area, area * depth];
+}
+
+const calcValue = calc(2,7,5);
+console.log(`The area of the box is: ${calcValue[0]}\nThe volume of the box is: ${calcValue[1]}`);
+//------------------------------------------------------------------------------------------------------------------
