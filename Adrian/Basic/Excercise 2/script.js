cb4a1252bb0c7db3ab48c840eb133ae23ed6d472
Adrian Pedziwiatr <adrianpe092@gmail.com>
2022-01-27 11:22:13 +0100

    excercise 2 and 3 done

--- Adrian/Basic/Excercise 2/script.js
+++ Adrian/Basic/Excercise 2/script.js
@@ -1,3 +1,12 @@
-function ageCalculator (birthYear, currentYear) {
-    console.log(`You are either`)
-}
+const ageCalculator1 = (function (birthYear, currentYear) {
+    return birthYear - currentYear;
+})(2022, 1993);
+
+const ageCalculator2 = (function (birthYear, currentYear) {
+    return birthYear - currentYear;
+})(2022, 1994);
+
+
+console.log(`You are either ${ageCalculator1} or ${ageCalculator2}`);
+
+
