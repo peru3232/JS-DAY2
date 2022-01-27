3980473f2e9a6a1e0e8dfca54592e4ed09346816
Adrian Pedziwiatr <adrianpe092@gmail.com>
2022-01-27 14:59:30 +0100

    files new

---
+++ Adrian/Advanced/script.js
@@ -0,0 +1,3 @@
+const changeNumbers = (minutes) => {console.log(`${minutes} minutes = ${Math.floor(minutes/60)} hours and ${Math.floor(minutes - 180)} minutes.`);}
+
+changeNumbers(200)
--- Adrian/Intermediate/Excercise 1/script.js
+++ Adrian/Intermediate/Excercise 1/script.js
@@ -1,6 +1,2 @@
-function capitalize(String) {
-    return String.chartAt(0).toUpperCase() + String.slice(1);
-}
 
-capitalize('i am a web developer');
 
